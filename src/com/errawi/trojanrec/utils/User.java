50fcdaac009f82e2e78f0e9a5a319121403de375
rvsahu <rvsahu@usc.edu>
2022-03-28 03:53:37 -0700

    User.java synchronised with client side version

--- src/com/errawi/trojanrec/utils/User.java
+++ src/com/errawi/trojanrec/utils/User.java
@@ -1,90 +1,98 @@
 package com.errawi.trojanrec.utils;
 
-public class User {
-	/**
-	 * The name of the user, first name followed by last name (separated with a
-	 * space).
-	 */
-	private String name;
+import java.io.Serializable;
 
-	/**
-	 * User's USC net ID. This is what users initially use to log in, and this will
-	 * be used for communicating with the server.
-	 */
-	private String netID;
+public class User implements Serializable {
 
-	/**
-	 * User's USC student ID. Needs to be a long as an int will not be able to store
-	 * many USC student IDs (Integer.MAX_VALUE is smaller than a lot of them, mine
-	 * for example).
-	 */
-	private long studentID;
+    private static final long serialVersionUID = 28932022L;
 
-	/**
-	 * User's photo. Not quite sure how to implement this yet, for now is a String
-	 * that is meant to be a URL to that image.
-	 */
-	private String userPhoto;
+    /**
+     * The name of the user, first name followed by last name (separated with a space).
+     */
+    private String name;
 
-	public User() {
-		name = "";
-		netID = "";
-		studentID = -1;
-		userPhoto = "";
-	}
+    /**
+     * User's USC net ID. This is what users initially use to log in, and this will be used
+     * for communicating with the server.
+     */
+    private String netID;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * User's USC student ID. Needs to be a long as an int will not be able to store many
+     * USC student IDs (Integer.MAX_VALUE is smaller than a lot of them, mine for example).
+     */
+    private long studentID;
 
-	public String getName() {
-		return this.name;
-	}
+    /**
+     * User's photo. Not quite sure how to implement this yet, for now is a String that
+     * is meant to be a URL to that image.
+     */
+    private String userPhoto;
 
-	public void setStudentID(long studentID) {
-		this.studentID = studentID;
-	}
+    public User() {
+        name = "";
+        netID = "";
+        studentID = -1;
+        userPhoto = "";
+    }
 
-	public long getStudentID() {
-		return this.studentID;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getNetID() {
-		return this.netID;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public void setNetID(String netID) {
-		this.netID = netID;
-	}
+    public void setStudentID(long studentID) {
+        this.studentID = studentID;
+    }
 
-	/**
-	 * Sets a URI to the user's photo.
-	 * 
-	 * @param userPhoto A URI to the user's photo.
-	 */
-	public void setUserPhoto(String userPhoto) {
-		this.userPhoto = userPhoto;
-	}
+    public long getStudentID() {
+        return this.studentID;
+    }
 
-	/**
-	 * Returns a URI to the user's photo.
-	 * 
-	 * @return A URI to the user's photo.
-	 */
-	public String getUserPhoto() {
-		return this.userPhoto;
-	}
+    public String getNetID() {
+        return this.netID;
+    }
 
-	/**
-	 * Returns a boolean representing if the user is logged in or not. True if they
-	 * are, false otherwise.
-	 *
-	 * @return Whether the user is logged in or not.
-	 */
-	public boolean isLoggedIn() {
-		if (studentID == -1) {
-			return false;
-		}
-		return true;
-	}
+    public void setNetID(String netID) {
+        this.netID = netID;
+    }
+
+    /**
+     * Sets a URI to the user's photo.
+     * @param userPhoto  A URI to the user's photo.
+     */
+    public void setUserPhoto(String userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+
+    /**
+     * Returns a URI to the user's photo.
+     * @return     A URI to the user's photo.
+     */
+    public String getUserPhoto() {
+        return this.userPhoto;
+    }
+
+    /**
+     * Returns a boolean representing if the user is logged in or not. True if they are,
+     * false otherwise.
+     *
+     * @return     Whether the user is logged in or not.
+     */
+    public boolean isLoggedIn() {
+        System.out.println("checking login: " + studentID + " " + this.toString());
+        if (studentID == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public void login(String username)
+    {
+        setStudentID(1);
+        System.out.println("logging in: " + studentID + " " + this.toString());
+    }
 }
