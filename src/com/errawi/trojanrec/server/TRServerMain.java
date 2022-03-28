ba56df1365e4011afc81e89e0f461ff988a63078
rvsahu <rvsahu@usc.edu>
2022-03-27 18:25:24 -0700

    Removed code from ClientHandler and TRServerMain
    A lot of the code in this file is only relevant to the context which I lifted it from (a homework assignment from a previous class) and isn't needed here. Hence it has been removed.
    
    Some minor adjustments too, like changing the thread pool we use from fixed to cached.

--- src/com/errawi/trojanrec/server/TRServerMain.java
+++ src/com/errawi/trojanrec/server/TRServerMain.java
@@ -13,100 +13,31 @@ import java.util.concurrent.Executors;
 
 
 public class TRServerMain  {
+	public static final int SOCKET_PORT = 1337;
+	
 	public static void main(String[] args) {
 		/*
 		 *  Thread variables
 		 */
-		List<ClientHandler> cHandlers;
-		ExecutorService clientExec;
+		List<ClientHandler> clientHandlers = new ArrayList<>();
+		ExecutorService clientExecutor = Executors.newCachedThreadPool();
+		DatabaseHandler databaseHandler = new DatabaseHandler();
 		
+		//declare and initialise a server socket for this program
+		ServerSocket ss = new ServerSocket(SOCKET_PORT);
 		
-		/*
-		 *  Server variables
-		 */
-		ServerSocket ss;
-		
-		/*
-		 * Program Variables
-		 */
-		//three concurrent-index lists for the schedule
-		List<Integer> orderTime; //the time the order takes place
-		List<String> orderFrom; //the restaurant being ordered from
-		List<String> orderOf; //the item being ordered
-		
-		int totalDrivers; //number of drivers doing delivery
-		
-		//read and validate schedule info
-		while (true) { //loop until valid file given
-			//open a scanner for the schedule file
-			try {
-				//output message to console
-				System.out.println("What is the name of the schedule file?");
-				//get the filename from user input to console
-				String filename = console.next();
-				//create a scanner from the file name
-				scheInfo = new Scanner(new File(filename));
-			} catch (FileNotFoundException fe) {
-				System.out.print("That file does not exist. ");
-				continue;
-			}
-			//initialise lists (and clear them if the previous input was bad)
-			orderTime = new ArrayList<Integer>();
-			orderFrom = new ArrayList<String>();
-			orderOf = new ArrayList<String>();
-			boolean scheduleFail = false;
-			//parse the scanner and validate input
-			while (scheInfo.hasNext()) {
-				String order = scheInfo.nextLine();
-				String[] orderSplit = order.split(", ");
-				if (orderSplit.length != 3) {
-					System.out.print("That file is not properly formatted. ");
-					scheduleFail = true;
-					break;
-				}
-				//get and validate the ready time
-				String timeStr = orderSplit[0];
-				int time;
-				try {
-					time = Integer.parseInt(timeStr);
-				} catch (NumberFormatException ne) {
-					System.out.print("That file is not properly formatted. ");
-					scheduleFail = true;
-					break;
-				}
-				//add to orderTime
-				orderTime.add(time);
-				//no validation necessary for the other parts
-				//get restaurant, add to orderFrom
-				orderFrom.add(orderSplit[1]);
-				//get menu item, add to orderOf
-				orderOf.add(orderSplit[2]);
-			}
-			if (!scheduleFail) {
-				break;
-			}
-		}
-		
-		//initialise user location
+		//number of clients connected to the server
+		int connectedClients; //number of drivers doing delivery
 
-		//create database handler
 		DatabaseHandler databaseHandler = new DatabaseHandler();
 		//OrderHandler oHandler = new OrderHandler(orderFrom, orderOf, orderTime, HQLocation);
 		//OrderHandlerTester.Test(oHandler);
 		//initialise dHandlers
-		clientHandlers = new ArrayList<ClientHandler>();		
+		clientHandlers = new ArrayList<ClientHandler>();
 		
 		try {
-			//part 2: open socket, wait for enough drivers
-			ss = new ServerSocket(3456);
-			int connectedDrivers = 0;
-			
-			//output wait message
-			//
-			System.out.println("Waiting for drivers...");
 			//connect first driver
-			DriverHandler dh = new DriverHandler(ss.accept(), oHandler);
-			connectedDrivers += 1;
+			ClientHandler ch = new ClientHandler(ss.accept());
 			System.out.println("Connection from " + dh.getAddress());
 			dh.sendNeededDrivers(totalDrivers - connectedDrivers);
 			dHandlers.add(dh);
@@ -128,7 +59,7 @@ public class TRServerMain  {
 			oHandler.setStartTime(TimeFormatter.getStartTime());
 			//part 3: clients start deliveries
 			//set delivery start time
-			//add all the DriverHandlers to a thread executor
+			//add all the DriverHandlers to a thread 
 			for (int i = 0; i < dHandlers.size(); i += 1) {
 				driverExec.submit(dHandlers.get(i));
 			}
