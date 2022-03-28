2b35f606e0c1b7d45879f10d5c3f0435afe04963
rvsahu <rvsahu@usc.edu>
2022-03-28 03:54:29 -0700

    uncommented all code
    and fixed formatting issues

--- src/com/errawi/trojanrec/server/TRServerMain.java
+++ src/com/errawi/trojanrec/server/TRServerMain.java
@@ -10,34 +10,37 @@ import java.util.concurrent.Executors;
 
 public class TRServerMain {
 	public static final int SOCKET_PORT = 1337;
-	/**
-	 * public static void main(String[] args) { List<ClientHandler> clientHandlers =
-	 * new ArrayList<>(); ExecutorService clientExecutor =
-	 * Executors.newCachedThreadPool(); DatabaseHandler databaseHandler = new
-	 * DatabaseHandler();
-	 * 
-	 * //declare a server socket for this program, will be initialised later
-	 * ServerSocket serverSocket;
-	 * 
-	 * //number of clients connected to the server int connectedClients = 0;
-	 * 
-	 * 
-	 * //first initialise server socket try { serverSocket = new
-	 * ServerSocket(SOCKET_PORT); //if this works enter a perpetual loop
-	 * 
-	 * while (true) { try { ClientHandler newClientHandler = new
-	 * ClientHandler(serverSocket.accept(), databaseHandler);
-	 * clientHandlers.add(newClientHandler);
-	 * clientExecutor.submit(newClientHandler); connectedClients += 1; } catch
-	 * (Exception e) { //TOO: log the exception, printStackTrace() but to a file,
-	 * etc. }
-	 * 
-	 * } //shutdown executor and then wait until terminated
-	 * //clientExecutor.shutdown(); /* //while (!(clientExecutor.isTerminated())) {
-	 * // Thread.yield(); //}
-	 * 
-	 * } catch (SocketException se) { //TOO: log the exception, printStackTrace()
-	 * but to a file, etc. } catch (IOException ie) { //TOO: log the exception,
-	 * printStackTrace() but to a file, etc. } }
-	 */
+	public static void main(String[] args) { 
+		List<ClientHandler> clientHandlers = new ArrayList<>(); 
+		ExecutorService clientExecutor = Executors.newCachedThreadPool(); 
+		DatabaseHandler databaseHandler = new DatabaseHandler();
+		
+		//declare a server socket for this program, will be initialised later
+		ServerSocket serverSocket;
+		
+		//number of clients connected to the server
+		int connectedClients = 0;
+		
+		//first initialise server socket 
+		try { 
+			serverSocket = new ServerSocket(SOCKET_PORT); //if this works enter a perpetual loop
+			while (true) { 
+				try { 
+					ClientHandler newClientHandler = new ClientHandler(serverSocket.accept(), databaseHandler);
+					clientHandlers.add(newClientHandler);
+					clientExecutor.submit(newClientHandler); connectedClients += 1; 
+				} catch (Exception e) { 
+					//TODO: log the exception, printStackTrace() but to a file, etc. 
+				}
+			} 
+		//shutdown executor and then wait until terminated
+		//clientExecutor.shutdown(); /* //while (!(clientExecutor.isTerminated())) {
+		// Thread.yield();
+		} catch (SocketException se) { 
+			//TOO: log the exception, printStackTrace() but to a file, etc. 
+		} catch (IOException ie) { 
+			//TOO: log the exception, printStackTrace() but to a file, etc. 
+		} 
+	}
 }
+	 
