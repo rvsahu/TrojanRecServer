a772fd20288ddc8043507b02982920d9a61c3420
rvsahu <rvsahu@usc.edu>
2022-03-29 13:02:51 -0700

    Lots more debug printing

--- src/com/errawi/trojanrec/server/TRServerMain.java
+++ src/com/errawi/trojanrec/server/TRServerMain.java
@@ -18,36 +18,60 @@ public class TRServerMain {
 		DatabaseHandler databaseHandler = new DatabaseHandler();
 		
 		//declare a server socket for this program, will be initialised later
-		ServerSocket serverSocket;
+		ServerSocket serverSocket = null;
 		
 		//first initialise server socket 
 		try {
 			System.out.println("Initialising server socket...");
 			serverSocket = new ServerSocket(SOCKET_PORT); //if this works enter a perpetual loop
-			System.out.println("Listening for connections..."); //TODO: output this to a log file too
-			while (true) { 
-				try { 
-					Socket newSocket = serverSocket.accept();
-					ClientHandler newCH = createClientHandler(newSocket, databaseHandler);
-					clientHandlers.add(newCH);
-					clientExecutor.submit(newCH); 
-				} catch (Exception e) { 
-					//TODO: log the exception, printStackTrace() but to a file, etc. 
-				}
-			} 
-		//shutdown executor and then wait until terminated
-		//clientExecutor.shutdown(); /* //while (!(clientExecutor.isTerminated())) {
-		// Thread.yield();
-		} catch (SocketException se) { 
-			//TOO: log the exception, printStackTrace() but to a file, etc. 
-		} catch (IOException ie) { 
-			//TOO: log the exception, printStackTrace() but to a file, etc. 
+		} catch (SocketException se) {
+			System.err.println("Socket exception while initialising server socket!");
+			se.printStackTrace();
+			closeSocket(serverSocket);
+			System.exit(-1);
+			//TODO: log this to a file
+		} catch (IOException ioe) {
+			System.err.println("IO exception while initialising server socket!");
+			ioe.printStackTrace();
+			closeSocket(serverSocket);
+			System.exit(-1);
+			//TODO: log this to a file
 		} 
+		System.out.println("Listening for connections..."); //TODO: output this to a log file too
+		while (serverSocket != null) { 
+			try {
+				Socket newSocket = serverSocket.accept();
+				ClientHandler newCH = createClientHandler(newSocket, databaseHandler);
+				clientHandlers.add(newCH);
+				clientExecutor.submit(newCH); 
+			} catch (IOException ioe) {
+				System.err.println("IO Exception accepting new socket connection!");
+				ioe.printStackTrace();
+				closeSocket(serverSocket);
+				System.exit(-1);
+			}
+		}
+		
+		System.out.println("Shouldn't ever really get here, but closing socket");
+		closeSocket(serverSocket);
 	}
 	
 	private static ClientHandler createClientHandler(Socket socket, DatabaseHandler dbHandler) {
 		System.out.println("New client connection!"); //TODO: output to a log file too
 		return new ClientHandler(socket, dbHandler);
+	}
+	
+	private static void closeSocket(ServerSocket serverSocket) {
+		if (serverSocket != null) {
+			try {
+				serverSocket.close();
+			} catch (IOException ioe) {
+				System.err.println("IO Exception closing server socket!");
+				ioe.printStackTrace();
+			}
+		} else {
+			System.err.println("Attempting to close null socket!");
+		}
 	}
 }
 	 
