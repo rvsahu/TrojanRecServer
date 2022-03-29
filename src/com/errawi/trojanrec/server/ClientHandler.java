23032858d8fab69b58839a84b2d492dc173b11c7
rvsahu <rvsahu@usc.edu>
2022-03-29 14:26:10 -0700

    Added CHECK_IF_LOGGED_IN function

--- src/com/errawi/trojanrec/server/ClientHandler.java
+++ src/com/errawi/trojanrec/server/ClientHandler.java
@@ -53,7 +53,7 @@ public class ClientHandler extends Thread {
      * Enumerates the functions that the server could need to perform for the client
      */
     private enum ServerFunction {
-        CONNECT, LOGIN, GET_PROFILE_INFO, GET_CURRENT_BOOKINGS,
+        CONNECT, LOGIN, CHECK_IF_LOGGED_IN, GET_PROFILE_INFO, GET_CURRENT_BOOKINGS,
         GET_PREVIOUS_BOOKINGS, GET_WAIT_LIST, GET_CENTRE_TIME_SLOTS, MAKE_BOOKING,
         CANCEL_BOOKING, CANCEL_WAIT_LIST, POLL_FOR_NOTIFICATIONS, CLOSE
     }
