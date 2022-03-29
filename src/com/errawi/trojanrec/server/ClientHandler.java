29b54423decb0337a341c5b04aa9a5e54ca691d0
rvsahu <rvsahu@usc.edu>
2022-03-28 19:55:49 -0700

    Handler implementation for some ClientRequests
    specifically initial connection, login, and retrieve profile info

--- src/com/errawi/trojanrec/server/ClientHandler.java
+++ src/com/errawi/trojanrec/server/ClientHandler.java
@@ -1,8 +1,7 @@
 package com.errawi.trojanrec.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.InetAddress;
@@ -10,28 +9,45 @@ import java.net.Socket;
 
 import com.errawi.trojanrec.utils.User;
 
+//TODO: for the debugging portion we'll likely want to implement some
+//logging to files
 public class ClientHandler extends Thread {
 	private Socket socket;
 
 	/**
 	 * An output stream to communicate objects to the client over.
+	 * Should always be ServerResponse objects.
 	 */
 	private ObjectOutputStream oos;
 
 	/**
-	 * An input stream to read strings from the client.
+	 * An input stream to read objects from the client.
+	 * Should always be ClientRequest objects.
 	 */
-	private BufferedReader br;
-	private DatabaseHandler databaseHandler;
+	private ObjectInputStream ois;
+	
+	/**
+	 * Synchronised database handler to interface between server threads 
+	 * and database while preventing race conditions.
+	 */
+	private DatabaseHandler dbHandler;
 
 	/**
 	 * User object representing the User that is logged into the (Android) TrojanRec
 	 * client that this thread is handling/communicating with.
+	 TODO: determine whether this field is necessary and if not, remove it
+	private User user;  
 	 */
-	private User user;
 	
-
-    /**
+	/**
+	 * A boolean representing whether the User of the client this handler is
+	 * communicating with is logged in. It is initially false and only becomes
+	 * true when the user successfully logs in. If not true, this handler will
+	 * not perform many requested functions.
+	 */
+	private boolean userAuthenticated;
+	
+	/**
      * Enumerates the functions that the server could need to perform for the client
      */
     private enum ServerFunction {
@@ -43,8 +59,8 @@ public class ClientHandler extends Thread {
     /**
      * Enumerates conditions with which the server could respond to the
      */
-    enum ResponseType {
-        CONNECTED, SUCCESS, FAIL, CLOSED
+    private enum ResponseType {
+        CONNECTED, AUTHENTICATED, UNAUTHENTICATED, SUCCESS, FAIL, CLOSED
     }
 
     /**
@@ -54,7 +70,7 @@ public class ClientHandler extends Thread {
      * exists as a nest private class because for security reasons we don't really
      * want another part of the program to be able to access/create these.
      */
-    private class ClientRequest implements Serializable {
+    private static class ClientRequest implements Serializable {
         private static final long serialVersionUID = 20220328L;
         /**
          * The function the server needs to perform, as enumerated by ServerFunctions.
@@ -126,15 +142,40 @@ public class ClientHandler extends Thread {
         }
     }
 
-    private class ServerResponse implements Serializable {
+    private static class ServerResponse implements Serializable {
+        private static final long serialVersionUID = 72002803L;
 
+        private ResponseType responseType;
+
+        private User user;
+
+        public ServerResponse(ResponseType responseType) {
+            this.responseType = responseType;
+        }
+
+        public void setResponse(ResponseType responseType) {
+            this.responseType = responseType;
+        }
+
+        public ResponseType responseType() {
+            return responseType;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+        public User getUser() {
+            return user;
+        }
     }
 	
-	public ClientHandler(Socket socket, DatabaseHandler databaseHandler) {
+	public ClientHandler(Socket socket, DatabaseHandler dbHandler) {
 		this.socket = socket;
-		this.databaseHandler = databaseHandler;
+		userAuthenticated = false;
+		this.dbHandler = dbHandler;
 		try {
-			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			ois = new ObjectInputStream(socket.getInputStream());
 			oos = new ObjectOutputStream(socket.getOutputStream());
 		} catch (IOException ie) {
 			ie.printStackTrace();
@@ -146,34 +187,155 @@ public class ClientHandler extends Thread {
 	}
 
 	public void run() {
-		//accept the first communication
+		//accept the first communication, basically a handshake.
+		//once this is done successfully, enter infinite loop.
+		ClientRequest currReq; //used to reference current client request
+		ServerResponse currResp; //references response to current request, may be sent or unsent
 		
+		try {
+			currReq = (ClientRequest)ois.readObject();
+			if (currReq.getFunction() == ServerFunction.CONNECT) {
+				//expected input, send back connection successful to
+				//complete handshake
+				currResp = new ServerResponse(ResponseType.CONNECTED);
+				oos.writeObject(currResp);
+			} else {
+				//received another request when it should have been CONNECT
+				//send a CLOSED type server response and close connection
+				sendClosedResponse();
+				//exit run() (which ends thread)
+				return;
+			}
+		} catch (ClassCastException cce) {
+			//object sent was not a ClientRequest
+			//send a CLOSED type server response and close connection
+			sendClosedResponse();
+			//exit run() (which ends thread)
+			return;
+		} catch (ClassNotFoundException cnfe) {
+			//not quite sure how we would get here but compiler says need to check for it
+			//send a CLOSED type server response and close connection
+			sendClosedResponse();
+			//exit run() (which ends thread)
+			return;
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			return;
+		}
 		
+		//connection successful. now thread enters main loop where it handles
+		//all requests
 		while (true) {
 			try {
-				
-				// DeliveryInformation info = oHandler.getReadyOrders();
-				// send orders to driver
-				// System.out.println(TimeFormatter.getTimeString() + " sent driver delivery
-				// instructions");
-				// oos.writeObject(info);
-				// check if info was null
-				// if (info == null) {
-				// it was, all orders are complete, break
-				// break;
-				// }
-				// wait for driver to return
-				String response = br.readLine();
-				if (response.equals("done")) {
-					continue;
-				} else {
-					break;
+				currReq = (ClientRequest)ois.readObject();
+				if (currReq.getFunction() == ServerFunction.LOGIN) {
+					//authenticate user
+					userAuthenticated = dbHandler.authenticateUser(currReq.getUser().getNetID(), currReq.getUserPassword());
+					
+					if (userAuthenticated) {
+						//send AUTHENTICATED response
+						currResp = new ServerResponse(ResponseType.AUTHENTICATED);
+						oos.writeObject(currResp);		
+					} else {
+						//send UNAUTHENTICATED response
+						sendUnauthenticatedResponse();
+					}
+				} else if (currReq.getFunction() == ServerFunction.CLOSE) {
+					sendClosedResponse();
+				} else if (!userAuthenticated) {
+					//return UNAUTHENTICATED response, meaning the server
+					//will not perform the client request because the User is not logged in
+					sendUnauthenticatedResponse();
+				} else if (currReq.getFunction() == ServerFunction.GET_PROFILE_INFO) {
+					//get server side User object from client side user's net ID
+					User serverSideUser = dbHandler.retrieveUser(currReq.getUser().getNetID());
+					//check if serverSideUser actually exists (studentID should not be -1)
+					if (serverSideUser.getStudentID() != -1) {
+						//send server side user back to client
+						currResp = new ServerResponse(ResponseType.SUCCESS); //create response with SUCCESS type
+						currResp.setUser(serverSideUser); //add server-side user to response
+						oos.writeObject(currResp); //send response
+					} else {
+						//send fail response back to client
+						sendFailResponse();
+					}
+				} else if (currReq.getFunction() == ServerFunction.GET_CURRENT_BOOKINGS) {
+					
 				}
-			} catch (Exception e) {
-				System.err.println("General error sending driver deliveries!");
-				e.printStackTrace();
-				break;
+			} catch (ClassCastException cce) {
+				//object sent was not a ClientRequest
+				//send a CLOSED type server response and close connection
+				sendClosedResponse();
+				//exit run() (which ends thread)
+				return;
+			} catch (ClassNotFoundException cnfe) {
+				//not quite sure how we would get here but compiler says need to check for it
+				//send a CLOSED type server response and close connection
+				sendClosedResponse();
+				//exit run() (which ends thread)
+				return;
+			} catch (IOException ioe) {
+				//some error reading from input stream. errors sending back would be handled
+				//further in. we should print stack trace and then end this thread.
+				ioe.printStackTrace();
+				return;
 			}
+		}
+	}
+	
+	/**
+	 * Below are cookie-cutter responses that may need to be sent, specifically
+	 * FAIL, CLOSED, and UNAUTHENTICATED. This may not be as useful for some of
+	 * the other response types as extra information would be added to the
+	 * ServerResponse objects. 
+	 */
+	
+	/**
+	 * Sends a server response with type UNAUTHENTICATED back to the client
+	 * 
+	 * @return     True if it could be sent back successfully, false otherwise.
+	 */
+	private boolean sendUnauthenticatedResponse() {
+		ServerResponse response = new ServerResponse(ResponseType.UNAUTHENTICATED);
+		try {
+			oos.writeObject(response); //send UNAUTHENTICATED response
+			return true;
+		} catch (IOException ioe) {
+			//couldn't successfully send back response, just return
+			ioe.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Sends a CLOSED response back to the client, and then closes the socket
+	 */
+	private void sendClosedResponse() {
+		ServerResponse response = new ServerResponse(ResponseType.CLOSED);
+		try {
+			oos.writeObject(response); //send CLOSED response
+			socket.close(); //close socket
+		} catch (IOException ioe) {
+			//couldn't successfully send back response, just return
+			ioe.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Sends a FAIL response back to the client, and returns whether sending it was
+	 * successful or not
+	 * 
+	 * @return     True if a FAIL response was sent successfully, false otherwise.
+	 */
+	private boolean sendFailResponse() {
+		ServerResponse response = new ServerResponse(ResponseType.CLOSED);
+		try {
+			oos.writeObject(response); //send FAIL response
+			return true;
+		} catch (IOException ioe) {
+			//couldn't successfully send back response, return false
+			ioe.printStackTrace();
+			return false;
 		}
 	}
 }
