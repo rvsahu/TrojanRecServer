5e053fa0dc83c26e222844918384f8670a748e81
rvsahu <rvsahu@usc.edu>
2022-04-10 16:55:16 -0700

    Add test class for TestClientHandler

---
+++ src/com/errawi/trojanrec/testing/TestClientHandler.java
@@ -0,0 +1,14 @@
+package com.errawi.trojanrec.testing;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+/**
+ * A class to test the outputs of 
+ * 
+ * @author RSahu
+ */
+public class TestClientHandler {
+
+}
