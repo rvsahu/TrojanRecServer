bb68758aacb443baae850eef2d1b164f7aa6774e
rvsahu <rvsahu@usc.edu>
2022-04-23 17:50:04 -0700

    Added NO_ACTION response
    NO_ACTION response means that no action was taken. This is different from FAIL in that fail means an action was taken but something went wrong. We would do NO_ACTION if, say, we tried to cancel a booking that doesn't exist.

--- src/com/errawi/trojanrec/utils/ResponseType.java
+++ src/com/errawi/trojanrec/utils/ResponseType.java
@@ -4,5 +4,5 @@ package com.errawi.trojanrec.utils;
  * Enumerates conditions with which the server could respond to the
  */
 public enum ResponseType {
-    CONNECTED, AUTHENTICATED, UNAUTHENTICATED, SUCCESS, FAIL, CLOSED
+    CONNECTED, AUTHENTICATED, UNAUTHENTICATED, SUCCESS, FAIL, NO_ACTION, CLOSED
 }
