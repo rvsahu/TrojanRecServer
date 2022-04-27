a0086113595ec8e4e51b9c5a54397b1f292cb63b
rvsahu <rvsahu@usc.edu>
2022-04-26 19:13:06 -0700

    add skeleton for notification bank

---
+++ src/com/errawi/trojanrec/utils/NotificationBank.java
@@ -0,0 +1,13 @@
+package com.errawi.trojanrec.utils;
+
+import java.util.List;
+
+public class NotificationBank {
+	public NotificationBank() {
+		//skeleton
+	}
+	
+	public boolean addUserNotifs(List<String> listOfUsers) {
+		return false;
+	}
+}
