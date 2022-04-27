b08bd59e72ae5214cb054a2d61eb049b752e4128
rvsahu <rvsahu@usc.edu>
2022-04-26 21:33:18 -0700

    Added field, getter, and setter for opened time slots to ServerResponse
    Opened time slots are slots for which the client was waitlisted and had since opened up.

--- src/com/errawi/trojanrec/utils/ServerResponse.java
+++ src/com/errawi/trojanrec/utils/ServerResponse.java
@@ -2,6 +2,7 @@ package com.errawi.trojanrec.utils;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ServerResponse implements Serializable {
         private static final long serialVersionUID = 72002803L;
@@ -11,7 +12,13 @@ public class ServerResponse implements Serializable {
         private User user;
 
         private ArrayList<Reservation> bookings;
-
+        
+        /**
+         * Slots that the user was waitlisted that had opened up.
+         * We send this to client so user can be notified of it.
+         */
+        private List<Reservation> openedSlots;
+        
         private ArrayList<String> timeslots;
 
         public ServerResponse(ResponseType responseType) {
@@ -48,5 +55,13 @@ public class ServerResponse implements Serializable {
 
         public ArrayList<String> getTimeslots() {
             return timeslots;
+        }
+        
+        public void setOpenedSlots(List<Reservation> openedSlots) {
+        	this.openedSlots = openedSlots;
+        }
+        
+        public List<Reservation> getOpenedSlots() {
+        	return openedSlots;
         }
     }
