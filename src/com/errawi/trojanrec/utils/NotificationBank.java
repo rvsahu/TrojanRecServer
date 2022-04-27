2f780903a733e82a05b5ea64724a1d7a27d3d9f2
rvsahu <rvsahu@usc.edu>
2022-04-26 19:30:03 -0700

    Filled in skeletons for NotificationBank
    Also implemented a couple short ones (constructor, getter)

--- src/com/errawi/trojanrec/utils/NotificationBank.java
+++ src/com/errawi/trojanrec/utils/NotificationBank.java
@@ -1,13 +1,31 @@
 package com.errawi.trojanrec.utils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NotificationBank {
+	/**
+	 * Maps user (by their string Net ID) to a list of slots which they waitlisted
+	 * for and then 
+	 */
+	private Map<String, List<Reservation> > userNotifs;
+	
 	public NotificationBank() {
-		//skeleton
+		userNotifs = new HashMap<String, List<Reservation> >();
 	}
 	
-	public boolean addUserNotifs(List<String> listOfUsers) {
+	public synchronized boolean addUserNotifs(List<String> listOfUsers, Reservation timeslot) {
 		return false;
+	}
+	
+	private boolean addUserNotifs(String netID, Reservation timeslot) {
+		return false;
+	}
+	
+	public synchronized List<Reservation> getUserNotifs(String netID) {
+		if (netID == null)
+			return null;
+		return userNotifs.get(netID);
 	}
 }
